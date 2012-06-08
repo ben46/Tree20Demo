db53e40c539b3bf6fbb88cfc63c9d20ef1cc3f37
ben46 <ben02060846@gmail.com>
2012-06-08 10:54:23 +0800

    Initial Commit

---
+++ Tree20Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tree20Demo
+//
+//  Created by nobby heell on 6/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ Tree20Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tree20Demo
+//
+//  Created by nobby heell on 6/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
